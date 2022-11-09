6eda704c0ccc49e78bd652c02fdcb80ad8edf322
daren-scerri <daren.scerri@mcast.edu.mt>
2022-11-09 12:28:06 +0100

    Added walls

--- SuperCannon_61A_22_23/Assets/Scripts/GameData.cs
+++ SuperCannon_61A_22_23/Assets/Scripts/GameData.cs
@@ -4,6 +4,27 @@ using UnityEngine;
 
 public abstract class GameData : MonoBehaviour
 {
+    public static float XMin
+    {
+        get { return Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).x; }
+    }
+
+    public static float XMax
+    {
+        get { return Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 0)).x; }
+    }
+
+    public static float YMin
+    {
+        get { return Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).y; }
+    }
+
+    public static float YMax
+    {
+        get { return Camera.main.ViewportToWorldPoint(new Vector3(0, 1, 0)).y; }
+    }
+
+
     public static Vector3 MousePos
         {
         get { return GetMousePos(); }
@@ -18,3 +39,6 @@ public abstract class GameData : MonoBehaviour
  
 
 }
+
+
+
---
+++ SuperCannon_61A_22_23/Assets/Scripts/SetupScene.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetupScene : MonoBehaviour
+{
+
+    GameObject vertWallprefab, horizWallprefab;
+
+    void Start()
+    {
+        vertWallprefab = Resources.Load("prefabs/VerticalWall") as GameObject;
+        horizWallprefab = Resources.Load("prefabs/HorizWall") as GameObject;
+
+        Instantiate(vertWallprefab, new Vector3(GameData.XMin - 4, 0, 0), Quaternion.identity);
+        Instantiate(vertWallprefab, new Vector3(GameData.XMax + 4, 0, 0), Quaternion.identity);
+        Instantiate(horizWallprefab, new Vector3(0, GameData.YMin - 4, 0), Quaternion.identity);
+        Instantiate(horizWallprefab, new Vector3(0, GameData.YMax + 4, 0), Quaternion.identity);
+    }
+
+
+}
