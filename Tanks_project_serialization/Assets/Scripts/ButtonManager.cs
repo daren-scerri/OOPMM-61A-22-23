e804ae9358c446108d2c40ea15f0517a8da2c013
daren-scerri <daren.scerri@mcast.edu.mt>
2023-01-11 11:50:08 +0100

    Added event system button management in a Welcome scene

---
+++ Tanks_project_serialization/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+
+    [SerializeField] Button startButton, exitButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton.onClick.AddListener(StartGame);
+        exitButton.onClick.AddListener(QuitGame);
+    }
+
+    void StartGame()
+    {
+        Debug.Log("You have clicked the start button");
+        SceneManager.LoadScene("GameScene");
+    }
+
+    void QuitGame()
+    {
+        Debug.Log("You have clicked the exit button");
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+         Application.OpenURL(webplayerQuitURL);
+#else
+         Application.Quit();
+#endif
+    }
+}
